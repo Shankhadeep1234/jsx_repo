f279538104b1f97cf5c33e262d6aa45b45b2ee85
Shankhadeep1234 <shankhadeepraj@gmail.com>
2022-08-09 14:01:29 +0530

    jsx tuts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
+import React from "react";
 
-class App extends Component {
-  render() {
-    return <div>Hello I am online, now what?</div>;
+function App() {
+  let message = "Hi There";
+  if (Math.random() > 0.5) {
+    message = "I am from Random";
   }
+  return <div>{message}</div>;
 }
 
 export default App;
