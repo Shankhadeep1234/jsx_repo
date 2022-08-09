473c3dec78a78b22a6bbc78b2c9395c8383794e4
Shankhadeep1234 <shankhadeepraj@gmail.com>
2022-08-09 20:57:51 +0530

    added style in input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 
 function App() {
-  let message = "Hi There";
-  if (Math.random() > 0.5) {
-    message = "I am from Random";
-  }
-  return <div>{message}</div>;
+  let inputType = "number";
+  let maxValue = 5;
+  return <input type={inputType} max={maxValue} style={{ color: "red" }} />;
 }
 
 export default App;
